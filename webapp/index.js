769f16f9a51ce8e5b05a9fa98c826e9c2f641589
cthompson4 <connort9345@gmail.com>
2023-01-30 12:43:43 -0600

    Added basic templates

---
+++ webapp/index.js
@@ -0,0 +1,13 @@
+
+
+// create app with express
+const express = require('express')
+const app = express()
+var url = require('url')
+app.use(express.static(__dirname + '/static'))
+const port = process.env.PORT || 8080;
+
+// listen on the port
+app.listen(port, () => console.log(
+  `Express started on http://localhost:${port}; ` +
+  `press Ctrl-C to terminate.`))
