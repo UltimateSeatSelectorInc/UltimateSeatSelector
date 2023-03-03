1b1c31639a7ff75ba2f692606ba24e1234d22174
Vincent Cortesi <vincentcortesi@gmail.com>
2023-03-02 22:22:01 -0600

    Added lectern seat popup

--- code/src/Map.jsx
+++ code/src/Map.jsx
@@ -1,38 +1,51 @@
-import React, { useState } from 'react';
-import Modal from 'react-modal';
+import React, { useState } from "react";
+import Modal from "react-modal";
 
 function Map(props) {
   const [hover, setHover] = useState(false);
   const [wasSelected, setWasSelected] = useState(false);
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [lecternModalIsOpen, setlecternModalIsOpen] = useState(false);
 
-  // function that changes color of the square on hover
   function handleHover() {
     setHover(true);
   }
 
-  // function that changes color of the square on click
-  function handleClick(){
+  function handleClick() {
     const index = props.index;
-    if (!props.chosen || props.seatStyle === index) {
-        props.updateStyle(index);
-        setModalIsOpen(true);
+    if (props.index === 30) { // Checks if lectern seat is clicked for lectern popup
+      props.updateStyle(index);
+      console.log("lectern seat selected!");
+      setlecternModalIsOpen(true);
+    } else if (!props.chosen || props.seatStyle === index) { // If not, display regular popup
+      props.updateStyle(index);
+      console.log("Regular seat selected!");
+      setModalIsOpen(true);
     }
-    if (props.chosen || props.seatStyle === index) {
+    if (props.chosen || props.seatStyle === index) {// Checks if lectern seat is being deselected
+      if(props.index===30){
+        console.log(props.index)
         props.updateStyle(null);
+        setlecternModalIsOpen(false);
+      }
+      else{
+        props.updateStyle(null);
+        console.log("Seat deselected!");
         setModalIsOpen(false);
+      }
+
     }
-    
-}
+  }
 
   function closeModal() {
     setModalIsOpen(false);
+    setlecternModalIsOpen(false);
   }
 
   return (
     <>
       <text
-        display={hover || props.seatStyle === props.index ? 'block' : 'none'}
+        display={hover || props.seatStyle === props.index ? "block" : "none"}
         x={props.x}
         y={props.y + 50}
         fontSize="10"
@@ -43,50 +56,76 @@ function Map(props) {
         {props.seat}
       </text>
       <rect
-        onMouseEnter={() => handleHover()} // call functions to handle hover/click
+        onMouseEnter={() => handleHover()}
         onMouseLeave={() => setHover(false)}
-        onClick={() => handleClick()} // will work even though already chosen... need to change this later
+        onClick={() => handleClick()}
         x={props.x}
         y={props.y}
         height={props.height}
         width={props.width}
         fill={
-          // fill color changes depending on selection, hover, occupancy
           props.chosen && hover && props.updateStyle
-            ? '#707070'
+            ? "#707070"
             : props.chosen && props.seatStyle === props.index
-            ? '#707070'
+            ? "#707070"
             : props.chosen
-            ? 'grey'
+            ? "grey"
             : hover || props.seatStyle === props.index
-            ? 'blue'
-            : 'black'
+            ? "blue"
+            : "black"
         }
       ></rect>
-        <Modal
-            isOpen={modalIsOpen}
-            onRequestClose={() => setModalIsOpen(false)}
-            contentLabel="Example Modal"
-            style={{
-                overlay: {
-                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    zIndex: 999
-                },
-                content: {
-                    width: '25%',
-                    height: '25%',
-                    top: '25%',
-                    left: '15%',
-                    transform: 'translate(-50%, -50%)'
-                }
-            }}
+      {lecternModalIsOpen ? (
+        <Modal // Lectern Modal - popup
+          isOpen={lecternModalIsOpen}
+          onRequestClose={() => setlecternModalIsOpen(false)}
+          contentLabel="Lectern Modal"
+          style={{
+            overlay: {
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              zIndex: 999,
+            },
+            content: {
+              width: "25%",
+              height: "25%",
+              top: "25%",
+              left: "15%",
+              transform: "translate(-50%, -50%)",
+            },
+          }}
         >
-            <h2>Seat {props.seat} Selected!</h2>
-            <button onClick={() => setModalIsOpen(false)}>Close</button>
+          <h2>This is a lectern seat!</h2>
+          <h3>Professor Credentials: </h3>
+          Name: <input type="text"></input><br></br><br></br>
+          Email: <input type="email"></input><br></br><br></br><br></br>
+
+          <button onClick={() => closeModal()}>Close</button>
         </Modal>
+      ) : (
+        <Modal // Regular Modal - popup
+          isOpen={modalIsOpen}
+          onRequestClose={() => closeModal()}
+          contentLabel="Example Modal"
+          style={{
+            overlay: {
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              zIndex: 999,
+            },
+            content: {
+              width: "25%",
+              height: "25%",
+              top: "25%",
+              left: "15%",
+              transform: "translate(-50%, -50%)",
+            },
+          }}
+        >
+          <h2>Seat {props.seat} Selected!</h2>
+          <button onClick={() => closeModal()}>Close</button>
+        </Modal>
+      )}
     </>
   );
 }
 
 export default Map;
-
